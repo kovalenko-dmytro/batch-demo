0e13f28cda11ca99484bf9209bc74de7aeaa6312
Kovalenko Dmytro <kovalenko.dm80@gmail.com>
2025-02-19 17:33:44 +0200

    [*] Refactored

--- src/main/java/com/gmail/apach/dima/batch_demo/BatchDemoApplication.java
+++ src/main/java/com/gmail/apach/dima/batch_demo/BatchDemoApplication.java
@@ -1,42 +1,12 @@
 package com.gmail.apach.dima.batch_demo;
 
-import com.gmail.apach.dima.batch_demo.core.base.common.constant.Delimiter;
-import com.gmail.apach.dima.batch_demo.infrastructure.common.constant.RunMode;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
+import com.gmail.apach.dima.batch_demo.infrastructure.common.run.ApplicationRunHandler;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BatchDemoApplication {
 
     public static void main(String[] args) {
-        run(args, getRunMode(args));
-    }
-
-    private static void run(String[] args, String runMode) {
-        switch (RunMode.from(runMode)) {
-            case CLI ->
-                new SpringApplicationBuilder(BatchDemoApplication.class)
-                    .web(WebApplicationType.NONE)
-                    .run(args);
-            case WEB ->
-                SpringApplication
-                    .run(BatchDemoApplication.class, args);
-            default ->
-                throw new IllegalStateException("Run mode: <" + runMode + "> has not supported");
-        }
-    }
-
-    private static String getRunMode(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalStateException("Application arguments should be set");
-        }
-
-        final var runMode = args[0].split(Delimiter.EQUAL)[1];
-        if (!RunMode.modes.contains(runMode)) {
-            throw new IllegalStateException("One of any <" + RunMode.modes + "> profiles should be set");
-        }
-        return runMode;
+        ApplicationRunHandler.run(args);
     }
 }
---
+++ src/main/java/com/gmail/apach/dima/batch_demo/infrastructure/common/run/ApplicationRunHandler.java
@@ -0,0 +1,39 @@
+package com.gmail.apach.dima.batch_demo.infrastructure.common.run;
+
+import com.gmail.apach.dima.batch_demo.BatchDemoApplication;
+import com.gmail.apach.dima.batch_demo.core.base.common.constant.Delimiter;
+import com.gmail.apach.dima.batch_demo.infrastructure.common.constant.RunMode;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ApplicationRunHandler {
+
+    public static void run(String[] args) {
+        final var runMode = getRunMode(args);
+        switch (RunMode.from(runMode)) {
+            case CLI -> new SpringApplicationBuilder(BatchDemoApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+            case WEB -> SpringApplication
+                .run(BatchDemoApplication.class, args);
+            default -> throw new IllegalStateException("Run mode: <" + runMode + "> has not supported");
+        }
+    }
+
+    private static String getRunMode(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalStateException("Application arguments should be set");
+        }
+
+        final var runMode = args[0].split(Delimiter.EQUAL)[1];
+        if (RunMode.modes.contains(runMode)) {
+            return runMode;
+        }
+
+        throw new IllegalStateException("One of any <" + RunMode.modes + "> profiles should be set");
+    }
+}
