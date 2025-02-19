ffe2283da2c1a955b10dd294a3fbc368132789d0
Kovalenko Dmytro <kovalenko.dm80@gmail.com>
2025-02-19 15:45:27 +0200

    [*] Refactored

--- src/main/java/com/gmail/apach/dima/batch_demo/core/job/import_csv_to_db/model/WorkLine.java
+++ src/main/java/com/gmail/apach/dima/batch_demo/core/job/import_csv_to_db/model/WorkLine.java
@@ -1,6 +1,8 @@
 package com.gmail.apach.dima.batch_demo.core.job.import_csv_to_db.model;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -12,7 +14,9 @@ public record WorkLine(
     @NotBlank
     @Size(max = 20)
     String fieldParam1,
+    @Max(100)
     Integer fieldParam2,
+    @Past
     LocalDateTime fieldParam3,
     Boolean fieldParam4
 ) {
