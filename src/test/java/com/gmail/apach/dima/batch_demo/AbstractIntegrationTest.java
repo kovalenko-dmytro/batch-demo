d88cd383895bef4b968e9e170abb3a37811bb6fb
Kovalenko Dmytro <kovalenko.dm80@gmail.com>
2025-02-20 10:11:46 +0200

    [*] Tests

--- src/test/java/com/gmail/apach/dima/batch_demo/AbstractIntegrationTest.java
+++ src/test/java/com/gmail/apach/dima/batch_demo/AbstractIntegrationTest.java
@@ -4,17 +4,33 @@ import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.junit5.api.DBRider;
 import com.gmail.apach.dima.batch_demo.infrastructure.common.constant.ActiveProfile;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
-@SpringBootTest(classes = BatchDemoApplication.class)
 @ActiveProfiles(ActiveProfile.TEST)
+@RunWith(SpringRunner.class)
+@SpringBatchTest
+@EnableAutoConfiguration
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @DBRider
 @DBUnit(
     caseInsensitiveStrategy = Orthography.LOWERCASE,
@@ -41,6 +57,17 @@ public abstract class AbstractIntegrationTest {
     static {
         POSTGRESQL_CONTAINER.start();
         LOCAL_STACK_CONTAINER.start();
+    }
+
+    @Autowired
+    protected JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    protected JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @AfterEach
+    public void cleanUp() {
+        jobRepositoryTestUtils.removeJobExecutions();
     }
 
     @BeforeAll
